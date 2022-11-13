aa75a8a95bb67e50aafeac6eca7d046f10aa5ddd
Douglas Schneider <dlschneider@proton.me>
2022-11-13 16:24:52 -0300

    Modificado para a tradução

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,20 @@
 import 'package:ecolinky/pages/splash_page.dart';
 import 'package:ecolinky/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 
 void main() {
-  runApp(const MyApp());
+  final FlutterI18nDelegate flutterDelegate = FlutterI18nDelegate(
+      translationLoader: FileTranslationLoader(
+          useCountryCode: false, fallbackFile: 'en', basePath: 'assets/i18n'));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp(flutterDelegate));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final FlutterI18nDelegate flutterDelegate;
+  MyApp(this.flutterDelegate);
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +28,16 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: SplashPage(),
+      localizationsDelegates: [
+        flutterDelegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en'),
+        const Locale('es'),
+        const Locale('pt'),
+      ],
     );
   }
 }
